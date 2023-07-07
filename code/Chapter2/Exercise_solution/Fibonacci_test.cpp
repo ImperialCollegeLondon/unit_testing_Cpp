b8102b89a68c44af785ca07d1b6996a1b5002a7f
Lokesh <lokesh.ragta@yahoo.com>
2023-07-07 15:46:57 +0100

    Added braces

--- code/Chapter2/Exercise_solution/Fibonacci_test.cpp
+++ code/Chapter2/Exercise_solution/Fibonacci_test.cpp
@@ -5,9 +5,9 @@
 // Returns the n-th term of the Fibonacci sequence.
 int recursive_fibonacci(int n)
 {
-    if(n < 0)
+    if(n < 0) {
         throw std::invalid_argument("Input must be a non-negative number");
-    else if (n <= 1) {
+    } else if (n <= 1) {
         return n;
     } else {
         return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2);
