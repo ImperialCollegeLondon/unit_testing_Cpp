e76d25149cf5bf035f5634a5702e45b394868aec
Diego Alonso Álvarez <6095790+dalonsoa@users.noreply.github.com>
2023-07-20 14:10:08 +0100

    Apply suggestions from code review
    Co-authored-by: Lokesh Kumar Ragta <72458522+lkr-ind@users.noreply.github.com>

--- code/Chapter5/employee_table.h
+++ code/Chapter5/employee_table.h
@@ -7,26 +7,27 @@ template <class GenericEmployee>
 class EmployeeTable
 {
 private:
-    std::vector<GenericEmployee> employees;
+    // See the note number 2 given in exercise_with_mocks on why we are using pointers here.
+    std::vector<GenericEmployee*> employees;
 
 public:
     // Member function to add employees into the table.
-    void addEmployee(const GenericEmployee &employee);
+    void addEmployee(GenericEmployee* employee)
+    {
+        employees.push_back(employee);
+    }
 
-    // Member function to remove employees into the table.
-    void removeEmployee(const std::string &employeeName);
-
-    // Member function to display information (names) of all employees in the table.
-    void displayEmployeesName() const;
-
-    // Function to check if the table is empty.
-    bool isEmpty() const;
-
-    // Function to get the number of entries in the table.
-    int getEntryCount() const;
+    // Other functions may or may not require changes.
+    // For brevity, we are using only two functions.
 
     // Member function to change the bonus to all employees.
-    void bumpSalaryBonus(const double newBonus);
+    void bumpSalaryBonus(const double newBonus)
+    {
+        for (auto& employee : employees)
+        {
+            employee->setIncreasedBasicBonus(newBonus);
+        }
+    }
 };
 
 #endif /* employee_table_h */
--- code/Chapter5/exercise_with_mocks.cpp
+++ code/Chapter5/exercise_with_mocks.cpp
@@ -6,20 +6,33 @@
 class MockEmployee
 {
 public:
-    MOCK_METHOD(void, setBasicBonus, (double));
+    MOCK_METHOD(void, setIncreasedBasicBonus, (double));
 };
 
 TEST(EmployeeTableTest, SetBasicBonusForEveryone)
 {
-    EmployeeTable<MockEmployee> table{EmployeeTable<MockEmployee>()};
+    EmployeeTable<MockEmployee> table; 
     double newBonus{2000};
-    MockEmployee employee1{MockEmployee()};
-    MockEmployee employee2{MockEmployee()};
+    
+    MockEmployee employee1;
+    MockEmployee employee2;
 
-    EXPECT_CALL(employee1, setBasicBonus(newBonus)).Times(1);
-    EXPECT_CALL(employee2, setBasicBonus(newBonus)).Times(1);
+    // Important Note 1
+    // As per the google mock documentation, the EXPECT_CALL() macro should be 
+    // set (i.e. expectations should be defined) before passing the mock objects
+    // to the API or code under test.
+    // Link:- http://google.github.io/googletest/gmock_for_dummies.html#using-mocks-in-tests
+    EXPECT_CALL(employee1, setIncreasedBasicBonus(newBonus)).Times(1);
+    EXPECT_CALL(employee2, setIncreasedBasicBonus(newBonus)).Times(1);
 
-    table.addEmployee(employee1);
-    table.addEmployee(employee2);
+    // Important Note 2
+    // A mock object cannot be directly copied to a vector because of the way the copy constructor is 
+    // defined for a mock class. 
+    // Instead pointer can be used to add the mock objects to the vector.
+    // Link:- https://github.com/google/googletest/issues/3734
+    table.addEmployee(&employee1);
+    table.addEmployee(&employee2);
+
+    // This is the function under test.
     table.bumpSalaryBonus(newBonus);
 };
